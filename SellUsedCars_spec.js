f25b38e46713ee0c098ea9f579aa84a550fafa88
SonalDalal <sonal.dalal@gmail.com>
2017-06-20 23:33:02 +0530

    Update SellUsedCars_spec.js

--- SellUsedCars_spec.js
+++ SellUsedCars_spec.js
@@ -18,8 +18,8 @@ describe('ClickIn - Post an ad for selling a Used Car', function() {
 	it('Navigate to Post an Ad page for selling a Used Car', function() {
 		browser.ignoreSynchronization = true;
 		//Open test application in new browser
-    	SellUsedCars.get(data.TestURL); 
-    	//Verify the Browser title
+    		SellUsedCars.get(data.TestURL); 
+    		//Verify the Browser title
    		browser.getTitle().then(function(text) {
    			expect(text).toBe("Free Classified Ads India | Free Ads Posting Classifieds India | ads India - Click.in");
 		});
@@ -28,36 +28,36 @@ describe('ClickIn - Post an ad for selling a Used Car', function() {
 	//Test Case 2: Verify that confirmation popup is displayed to set the City.
 	it('Verify that confirmation popup is displayed to set the City', function() {
 		//verify that the City selection default popup is displayed.
-    	expect(SellUsedCars.SelectCityPopup.isDisplayed()).toBe(true);
-    	//Verify the text on it.
-    	SellUsedCars.SelectCityPopup.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.CitySelectionText);
-    	});
-    	//Click on OK button to confirm the city.
-    	expect(SellUsedCars.OKButton.isDisplayed()).toBe(true);
-    	SellUsedCars.OKButton.click();
-    	//verify that the City selection default popup is closed.
-    	expect(SellUsedCars.SelectCityPopup.isDisplayed()).toBe(false);
+    		expect(SellUsedCars.SelectCityPopup.isDisplayed()).toBe(true);
+    		//Verify the text on it.
+    		SellUsedCars.SelectCityPopup.getText().then(function(text) {
+    			expect(text).toBe(SellUsedCars.CitySelectionText);
+    		});
+    		//Click on OK button to confirm the city.
+    		expect(SellUsedCars.OKButton.isDisplayed()).toBe(true);
+    		SellUsedCars.OKButton.click();
+    		//verify that the City selection default popup is closed.
+    		expect(SellUsedCars.SelectCityPopup.isDisplayed()).toBe(false);
 	});
 	
 	
 	//Test Case 3: Navigate to Post an Ad screen for Used Cars by clicking on "Post your Ad" link
 	it('Navigate to Post an Ad for Used Cars screen by clicking on "Post your Ad" link', function() {
 		//verify that "Post your Ad" link is displayed on landing page and click on it.
-    	expect(SellUsedCars.PostYourAdLink.isDisplayed()).toBe(true);
-    	SellUsedCars.PostYourAdLink.click();
-    	//Verify that Post an Ad overlay is displayed and user can select Automobiles module
-    	expect(SellUsedCars.PostAnAdOverlay.isDisplayed()).toBe(true);
-    	expect(SellUsedCars.AutomobilesModule.isDisplayed()).toBe(true);
-    	SellUsedCars.AutomobilesModule.click();
-    	//Verify that link to Sell Used Cars is displayed and click on that
-    	expect(SellUsedCars.UsedCarsLink.isDisplayed()).toBe(true);
-    	SellUsedCars.UsedCarsLink.click();
-    	//Verify that Post an Ad page is displayed.
-    	expect(SellUsedCars.PaAPageNavigationDisplay.isDisplayed()).toBe(true);
-    	SellUsedCars.PaAPageNavigationDisplay.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.PaAPageNavigationDisplayText);
-    	});
+    		expect(SellUsedCars.PostYourAdLink.isDisplayed()).toBe(true);
+    		SellUsedCars.PostYourAdLink.click();
+    		//Verify that Post an Ad overlay is displayed and user can select Automobiles module
+    		expect(SellUsedCars.PostAnAdOverlay.isDisplayed()).toBe(true);
+    		expect(SellUsedCars.AutomobilesModule.isDisplayed()).toBe(true);
+    		SellUsedCars.AutomobilesModule.click();
+    		//Verify that link to Sell Used Cars is displayed and click on that
+    		expect(SellUsedCars.UsedCarsLink.isDisplayed()).toBe(true);
+    		SellUsedCars.UsedCarsLink.click();
+    		//Verify that Post an Ad page is displayed.
+    		expect(SellUsedCars.PaAPageNavigationDisplay.isDisplayed()).toBe(true);
+    		SellUsedCars.PaAPageNavigationDisplay.getText().then(function(text) {
+    			expect(text).toBe(SellUsedCars.PaAPageNavigationDisplayText);
+    		});
 	});
 	
 	
@@ -75,62 +75,59 @@ describe('ClickIn - Post an ad for selling a Used Car', function() {
 		//Verify error messages for Car Brand
 		expect(SellUsedCars.SelectCarBrandErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectCarBrandErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectCarBrandErrMsgText);
-    	});
-    	//Verify error messages for Fuel Type
-    	expect(SellUsedCars.SelectFuelTypeErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectCarBrandErrMsgText);
+    		});
+    		//Verify error messages for Fuel Type
+    		expect(SellUsedCars.SelectFuelTypeErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectFuelTypeErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectFuelTypeErrMsgText);
-    	});
-    	//Verify error messages for Manufactured Year
-    	expect(SellUsedCars.SelectManufacturedYearErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectFuelTypeErrMsgText);
+    		});
+    		//Verify error messages for Manufactured Year
+    		expect(SellUsedCars.SelectManufacturedYearErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectManufacturedYearErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectManufacturedYearErrMsgText);
-    	});
-    	//Verify error messages for Kms Done
-    	expect(SellUsedCars.SelectKmsDoneErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectManufacturedYearErrMsgText);
+    		});
+    		//Verify error messages for Kms Done
+    		expect(SellUsedCars.SelectKmsDoneErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectKmsDoneErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectKmsDoneErrMsgText);
-    	});
-    	//Verify error messages for Expected Price
-    	expect(SellUsedCars.SelectExpectedPriceErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectKmsDoneErrMsgText);
+    		});
+    		//Verify error messages for Expected Price
+    		expect(SellUsedCars.SelectExpectedPriceErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectExpectedPriceErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectExpectedPriceErrMsgText);
-    	});
-    	//Verify error messages for Insurance Included
-    	expect(SellUsedCars.SelectInsuranceIncErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectExpectedPriceErrMsgText);
+    		});
+    		//Verify error messages for Insurance Included
+    		expect(SellUsedCars.SelectInsuranceIncErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectInsuranceIncErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectInsuranceIncErrMsgText);
-    	});
-    	//Verify error messages for Classified Heading
-    	expect(SellUsedCars.SelectClassifiedHeadErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectInsuranceIncErrMsgText);
+    		});
+    		//Verify error messages for Classified Heading
+    		expect(SellUsedCars.SelectClassifiedHeadErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectClassifiedHeadErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectClassifiedHeadErrMsgText);
-    	});
-    	//Verify error messages for Car Description
-    	expect(SellUsedCars.SelectCarDescriptionErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectClassifiedHeadErrMsgText);
+    		});
+    		//Verify error messages for Car Description
+    		expect(SellUsedCars.SelectCarDescriptionErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectCarDescriptionErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectCarDescriptionErrMsgText);
-    	});
-    	//Verify error messages for Email Id
-    	expect(SellUsedCars.SelectEmailIDErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectCarDescriptionErrMsgText);
+    		});
+    		//Verify error messages for Email Id
+    		expect(SellUsedCars.SelectEmailIDErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectEmailIDErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectEmailIDErrMsgText);
-    	});
-    	//Verify error messages for You Are
-    	expect(SellUsedCars.SelectYouAreErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectEmailIDErrMsgText);
+    		});
+    		//Verify error messages for You Are
+    		expect(SellUsedCars.SelectYouAreErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectYouAreErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectYouAreErrMsgText);
-    	});
-    	//Verify error messages for Locality
-    	expect(SellUsedCars.SelectLocalityErrMsg.isDisplayed()).toBe(true);
+    			expect(text).toBe(SellUsedCars.SelectYouAreErrMsgText);
+    		});
+    		//Verify error messages for Locality
+    		expect(SellUsedCars.SelectLocalityErrMsg.isDisplayed()).toBe(true);
 		SellUsedCars.SelectLocalityErrMsg.getText().then(function(text) {
-    		expect(text).toBe(SellUsedCars.SelectLocalityErrMsgText);
-    	});
+    			expect(text).toBe(SellUsedCars.SelectLocalityErrMsgText);
+    		});
  	
 	});
-	
-	
 	}); /* closing test data file*/
-
 });
